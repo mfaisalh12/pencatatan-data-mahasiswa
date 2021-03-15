4797adfa6bf82a522c4d01d343634a9b003cc138
Faisal <mfaisalhidayatullah@gmail.com>
2021-03-15 09:21:23 +0700

    style: change the tambah data title

--- resources/views/mhs/index.blade.php
+++ resources/views/mhs/index.blade.php
@@ -54,7 +54,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
